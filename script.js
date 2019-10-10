db46f27a14aa9a7c0515977182185625542a3fba
Haley Greer <haleygreer1@studentbody.byu.edu>
2019-10-10 17:46:04 +0000

    api works

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ document.getElementById("kanyeSubmit").addEventListener("click", function(event)
     event.preventDefault();
     
     const url = "https://api.kanye.rest";
+    console.log("1");
     fetch(url)
         .then(function(response) {
+            console.log("2");
             return response.json();
         }).then(function(json) {
-            console.log(json);
+            console.log(json.quote);
             let results = "";
-            results += "<h2>" + json.name + "</h2>";
+            console.log("3");
+            results += json.quote;
             document.getElementById("kanyeResults").innerHTML = results;
             console.log(response);
         });
-        
-        
-        
 });

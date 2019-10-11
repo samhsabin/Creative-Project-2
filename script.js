9f2c08928ec9cd140980d5738f4eb397e74dddee
Sam Sabin <samuelhsabin@gmail.com>
2019-10-11 20:46:16 +0000

    finished

--- script.js
+++ script.js
@@ -4,17 +4,14 @@ document.getElementById("kanyeSubmit").addEventListener("click", function(event)
     event.preventDefault();
     
     const url = "https://api.kanye.rest";
-    console.log("1");
     fetch(url)
         .then(function(response) {
-            console.log("2");
             return response.json();
         }).then(function(json) {
             console.log(json.quote);
             let results = "";
-            console.log("3");
-            results += json.quote;
+            results += '"' + json.quote + '"';
+            console.log(results);
             document.getElementById("kanyeResults").innerHTML = results;
-            console.log(response);
         });
 });
